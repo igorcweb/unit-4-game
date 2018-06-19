17b12550dc9421669bd8f8b7826db6a69bfd1beb
Igor Chernyshev <igorchern07@gmail.com>
2018-06-19 13:19:41 -0500

    finished functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,51 @@
-// Crystals 1-12
-// Number 19-120
 (function() {
-  $('span#number').text(Math.floor(Math.random() * 120) + 19);
+  let number;
+  let numberDisplay = $('span#number');
+  let wins = 0;
+  let losses = 0;
   const gold = $('img.gold');
   const blue = $('img.blue');
   const purple = $('img.purple');
   const green = $('img.green');
-  const crystal = $('div.crystal-bg');
-  const img = $('img');
-  $(crystal).on('click', img, function() {
-    console.log(this);
-  });
+  const crystals = [gold, blue, purple, green];
+  let score = 0;
+  const scoreDisplay = $('span#score');
+  const message = $('h3.message');
+
+  const game = {
+    reset: function reset() {
+      score = 0;
+      scoreDisplay.text(score);
+      number = Math.floor(Math.random() * 120) + 19;
+      numberDisplay.text(number);
+      $.each(crystals, function(index, crystal) {
+        crystal.value = Math.floor(Math.random() * 12) + 1;
+      });
+    },
+    play: function() {
+      $.each(crystals, function(index, crystal) {
+        crystal.on('click', function() {
+          const value = $(crystal.value)[0];
+          score += value;
+          console.log(value);
+          scoreDisplay.text(score);
+          if (score === number) {
+            message.text('You Win!');
+            wins++;
+            $('#wins').text(wins);
+            game.reset();
+          }
+          if (score > number) {
+            message.text('You Lose!');
+            losses++;
+            $('#losses').text(losses);
+            game.reset();
+          }
+        });
+      });
+    }
+  };
+
+  game.reset();
+  game.play();
 })();
